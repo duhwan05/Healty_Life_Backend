00072dbcae48924f8f9e452236418c9058b0cadb
kimjiisuuu <raina11170@gmail.com>
2024-06-26 15:22:19 +0900

    security test 메소드 추가

--- src/main/java/com/example/healthylife/service/UserService.java
+++ src/main/java/com/example/healthylife/service/UserService.java
@@ -4,15 +4,20 @@ import com.example.healthylife.entity.UserEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface UserService {
     //회원 전체조회
     List<UserEntity> userList();
+    // TODO
+    Optional<UserEntity> findUserById(String userId);
     //회원 등록
     UserEntity registerUser(UserEntity userEntity);
     //회원 수정
     UserEntity updateUser(UserEntity userEntity);
     //회원 삭제
     void deleteUserBySq(long userSq);
+
+
 }

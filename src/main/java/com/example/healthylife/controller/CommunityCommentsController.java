dca8140bfa198af292d0e0fcf0239cd061fc9b13
kimjiisuuu <raina11170@gmail.com>
2024-06-27 14:10:55 +0900

    controller url 변경 및 회원 단일조회 주석

--- src/main/java/com/example/healthylife/controller/CommunityCommentsController.java
+++ src/main/java/com/example/healthylife/controller/CommunityCommentsController.java
@@ -8,28 +8,28 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/api/v1/communityComments")
+@RequestMapping("/communityComments")
 @RestController
 @RequiredArgsConstructor
 public class CommunityCommentsController {
 
     private final CommunityCommentsService communityCommentsService;
 
-    @ApiOperation(value = "댓글 전체 조회")
+    @ApiOperation(value = "커뮤니티 댓글 전체 조회")
     @GetMapping("/all")
     public List<CommunityCommentsEntity> communityCommentsList(){
         List<CommunityCommentsEntity> communityCommentsEntities = communityCommentsService.communityCommentsList();
         return communityCommentsEntities;
     }
 
-    @ApiOperation(value = "댓글 작성")
+    @ApiOperation(value = "커뮤니티 댓글 작성")
     @PostMapping("/insert")
     public CommunityCommentsEntity insert(@RequestBody CommunityCommentsEntity communityCommentsEntity){
         return communityCommentsService.insertComments(communityCommentsEntity);
 
     }
 
-    @ApiOperation(value = "댓글 삭제")
+    @ApiOperation(value = "커뮤니티 댓글 삭제")
     @PostMapping("/delete")
     public Boolean delete(@RequestParam long commentsSq){
         communityCommentsService.deleteBySq(commentsSq);

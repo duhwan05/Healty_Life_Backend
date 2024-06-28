4eacded39b64260ec8ce2b58a6f72ea73335106d
승준 <132419579+seung-jun2@users.noreply.github.com>
2024-06-28 19:50:05 +0900

    Update CorsConfig.java

--- src/main/java/com/example/healthylife/config/CorsConfig.java
+++ src/main/java/com/example/healthylife/config/CorsConfig.java
@@ -10,26 +10,6 @@ public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
 
-        // 허용할 Origin 설정
-        config.addAllowedOrigin("http://ec2-43-201-150-178.ap-northeast-2.compute.amazonaws.com:8081/");
-        config.addAllowedOrigin("http://localhost:8081/swagger-ui/index.html");
-
-        // 모든 HTTP 메서드 허용
-        config.addAllowedMethod("*");
-
-        // 모든 헤더 허용
-        config.addAllowedHeader("*");
-
-        // 자격 증명 허용
-        config.setAllowCredentials(true);
-
-        // 모든 경로에 대해 위의 CORS 설정 적용
-        source.registerCorsConfiguration("/**", config);
-
-        // CORS 필터 반환
-        return new CorsFilter(source);
     }
 }

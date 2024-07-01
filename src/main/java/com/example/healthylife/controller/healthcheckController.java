3c379eae0e79beb72d207a2f48bb7e44157e2af1
승준 <132419579+seung-jun2@users.noreply.github.com>
2024-07-01 21:04:34 +0900

    Update healthcheckController.java

--- src/main/java/com/example/healthylife/controller/healthcheckController.java
+++ src/main/java/com/example/healthylife/controller/healthcheckController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class healthcheckController {
-
+    //d
     @GetMapping("/health-check")
     public ResponseEntity<Void> checkhealth(){
         return new ResponseEntity<>(HttpStatus.OK);
